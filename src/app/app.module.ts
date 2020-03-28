03c933743166ceaead4d95d4679bdcb0392cdcdf
Mamadou Billo Diallo <billodiallonet@gmail.com>
2020-03-28 14:23:32 +0300

    import and add formmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DiaryComponent } from './diary/diary.component';
 import { DiaryDetailComponent } from './diary-detail/diary-detail.component';
 import { StrikethroughDirective } from './strikethrough.directive';
+import { DateCountPipe } from './date-count.pipe';
+import { DiaryFormComponent } from './diary-form/diary-form.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     DiaryComponent,
     DiaryDetailComponent,
-    StrikethroughDirective
+    StrikethroughDirective,
+    DateCountPipe,
+    DiaryFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
