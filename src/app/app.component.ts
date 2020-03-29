318ef99900d76895d5adfbaea857ad8566b37a65
Mamadou Billo Diallo <billodiallonet@gmail.com>
2020-03-29 09:17:34 +0300

    correct spelling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { Diary } from './diary';
 export class AppComponent {
   diarys:Diary[] =  [
     new Diary(1, 'Doing Sport every weekend','to have good health',new Date(2020,3,14)),
-    new Diary(2,'Lear Angular and javascript','be sofware developer',new Date(2019,6,9)),
+    new Diary(2,'Learn Angular and javascript','be sofware developer',new Date(2019,6,9)),
     new Diary(3,'Reduce Stress in my life','good health',new Date(2022,1,12)),
     new Diary(4,'TRavel to USA for 2 weeks', 'holiday',new Date(2019,0,18)),
     new Diary(5,'Visit my family in Guinea in 2021','social ',new Date(2019,2,14)),
